6981aa2d6d4ffb8451314607aca354b92816ed7c
Matthias Schoettle <git@mattsch.com>
2025-06-10 11:36:43 -0400

    feat: ensure that a port is not already allocated (#39)

---
+++ extensions/network.py
@@ -0,0 +1,18 @@
+import socket
+from contextlib import closing
+
+from jinja2 import Environment
+from jinja2.ext import Extension
+
+
+# source: https://stackoverflow.com/a/35370008
+def is_port_available(port: int, host: str = 'localhost') -> bool:
+    """Check if a port is available on a given host (or localhost by default)."""
+    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
+        return sock.connect_ex((host, port)) != 0
+
+
+class NetworkExtension(Extension):
+    def __init__(self, environment: Environment):
+        super().__init__(environment)
+        environment.globals['is_port_available'] = is_port_available
