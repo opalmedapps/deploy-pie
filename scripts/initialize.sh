bb031d298b7782af52721cf60663e5a2f9aa37a9
Matthias Schoettle <git@mattsch.com>
2025-11-27 11:26:43 -0500

    fix: make sure the interface-engine user gets a password set

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Name: initialize.sh
 # Date Authored: 29 Oct 2024
-# Author: Kelly Agnew
+# Author: Kelly Agnew, Matthias Schoettle
 # Description: Ensure database schemas are fully updated and migrated, and have the minimum set of data for functionality
 
 set -euo pipefail
@@ -16,6 +16,8 @@ declare -a commands=(
     "docker compose run --rm db-management python -m db_management.run_sql_scripts QuestionnaireDB db_management/questionnairedb/data/initial/"
 
     "docker compose exec admin python manage.py initialize_data --listener-token=${LISTENER_TOKEN} --listener-registration-token=${LISTENER_REGISTRATION_TOKEN} --interface-engine-token=${INTERFACE_ENGINE_TOKEN} --opaladmin-backend-legacy-token=${ADMIN_TOKEN} --admin-password=${ADMIN_PASSWORD}"
+    # explicitly set a password for the interface-engine user to allow logins on admin-legacy
+    "docker compose exec admin python manage.py shell -c 'user = User.objects.get(username=\"interface-engine\"); user.set_password(${INTERFACE_ENGINE_PASSWORD}); user.save();'"
 )
 
 # Execute each command
