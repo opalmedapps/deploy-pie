2e8634b809f194435da2a118784002f2e3203bf4
Matthias Schoettle <git@mattsch.com>
2026-01-21 17:06:48 -0500

    add labs_login test and exclude tests directory

---
+++ tests/labs_login.py
@@ -0,0 +1,33 @@
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "requests>=2.32.5",
+#     "typer-slim>=0.21.1",
+# ]
+# ///
+import requests
+import typer
+
+
+def main(labs_password: str):
+    response = requests.post(
+        'http://localhost/opalAdmin/user/validate-login',
+        data={
+            'username': 'interface-engine',
+            'password': labs_password,
+        },
+        timeout=5,
+        allow_redirects=False,
+        # during CI there won't be a valid cert
+        verify=False,  # noqa: S501
+    )
+
+    if not response.ok:
+        print(f'Labs login failed: {response.status_code} {response.text}')
+        raise typer.Exit(code=1)
+
+    print('Labs login succeeded')
+
+
+if __name__ == '__main__':
+    typer.run(main)
