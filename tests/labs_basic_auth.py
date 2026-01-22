8104e8bbbd5e7325077eaafa0dfc7f359f875e9d
Matthias Schoettle <git@mattsch.com>
2026-01-22 11:18:48 -0500

    add test for labs basic auth

---
+++ tests/labs_basic_auth.py
@@ -0,0 +1,41 @@
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "requests>=2.32.5",
+#     "typer-slim>=0.21.1",
+# ]
+# ///
+import json
+from http import HTTPStatus
+
+import requests
+import typer
+
+
+def main(labs_password: str):
+    response = requests.post(
+        'https://opalmedapps.dev:444/opalAdmin/labs/api/processLabForPatient.php',
+        auth=('interface-engine', labs_password),
+        data=json.dumps({
+            'mrn': 'test',
+            'site': 'test',
+            'labOrders': [],
+        }),
+        timeout=5,
+        allow_redirects=False,
+        # during CI there won't be a valid cert
+        verify=False,  # noqa: S501
+    )
+
+    if (
+        response.status_code == HTTPStatus.BAD_REQUEST
+        and response.json().get('reason') == 'Opal patient with Mrn: test, Site: test not found.'
+    ):
+        print('Labs request with basic auth succeeded')
+    else:
+        print(f'Labs request with basic auth failed: {response.status_code} {response.text}')
+        raise typer.Exit(code=1)
+
+
+if __name__ == '__main__':
+    typer.run(main)
