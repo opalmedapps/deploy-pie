9c875b936e5fb3e74034cc92b2a3a570832e164f
Matthias Schoettle <git@mattsch.com>
2026-01-23 17:03:04 -0500

    add output for is_port_available

--- extensions/network.py
+++ extensions/network.py
@@ -8,6 +8,8 @@ from jinja2.ext import Extension
 # source: https://stackoverflow.com/a/35370008
 def is_port_available(port: int, host: str = 'localhost') -> bool:
     """Check if a port is available on a given host (or localhost by default)."""
+    print(f'Checking availability of port {port} on host {host}...')
+
     with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
         return sock.connect_ex((host, port)) != 0
 
