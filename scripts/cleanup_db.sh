9a4bc1c4e360a5aa6c71e1d7f60241ddcd0d9e2d
Matthias Schoettle <git@mattsch.com>
2026-01-27 16:35:43 -0500

    switch to mysql command

--- scripts/cleanup_db.sh
+++ scripts/cleanup_db.sh
@@ -3,14 +3,14 @@ set -euo pipefail
 
 echo "Connecting to DB server on ${DB_HOST}:${DB_PORT}..."
 echo "Dropping databases..."
-MYSQL_PWD=${DB_ROOT_PASSWORD} mariadb --protocol tcp --skip-ssl --user ${DB_ROOT_USER} --host ${DB_HOST} --port ${DB_PORT} <<'EOIF'
+MYSQL_PWD=${DB_ROOT_PASSWORD} mysql --protocol tcp --skip-ssl --user ${DB_ROOT_USER} --host ${DB_HOST} --port ${DB_PORT} <<'EOIF'
 DROP DATABASE IF EXISTS \`$DB_NAME\`;
 DROP DATABASE IF EXISTS \`OpalDB\`;
 DROP DATABASE IF EXISTS \`QuestionnaireDB\`;
 EOIF
 echo "Successfully dropped databases"
 echo "Dropping DB user "${DB_USER}"..."
-MYSQL_PWD=${DB_ROOT_PASSWORD} mariadb --protocol tcp --skip-ssl --user ${DB_ROOT_USER} --host ${DB_HOST} --port ${DB_PORT} <<'EOIF'
+MYSQL_PWD=${DB_ROOT_PASSWORD} mysql --protocol tcp --skip-ssl --user ${DB_ROOT_USER} --host ${DB_HOST} --port ${DB_PORT} <<'EOIF'
 DROP USER IF EXISTS '$DB_USER'@'%';
 FLUSH PRIVILEGES;
 EOIF
