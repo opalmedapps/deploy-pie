a8b97f480d6596005bde72522ab511c931640fe6
Matthias Schoettle <git@mattsch.com>
2026-02-02 10:45:30 -0500

    feat: add option to deploy ORMS (#106)

---
+++ scripts/init_orms.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -euo pipefail
+
+# renovate: datasource=docker depName=alpine
+ALPINE_VERSION="3.23.2"
+WAIT_FOR_IT_VERSION="latest"
+
+echo "Waiting for DB container to be ready..."
+docker run --rm --interactive --network opal-${ENVIRONMENT} chainguard/wait-for-it:${WAIT_FOR_IT_VERSION} --host="$DB_HOST" --port="$DB_PORT" --timeout=20
+
+echo "Running container for mysql-client..."
+docker run --rm --interactive \
+    --env DB_ROOT_USER=${DB_ROOT_USER} \
+    --env DB_ROOT_PASSWORD=${DB_ROOT_PASSWORD} \
+    --env DB_HOST=${DB_HOST} \
+    --env DB_USER=${DB_USER} \
+    --env DB_PASSWORD=${DB_PASSWORD} \
+    --network opal-${ENVIRONMENT} \
+    alpine:${ALPINE_VERSION} sh -s << EOF
+set -euo pipefail
+apk add --no-cache mysql-client
+echo "Connecting to DB server on ${DB_HOST}:${DB_PORT}..."
+echo "Creating ORMS DBs..."
+MYSQL_PWD=${DB_ROOT_PASSWORD} mariadb --protocol tcp --skip-ssl --user ${DB_ROOT_USER} --host ${DB_HOST} --port ${DB_PORT} <<'EOIF'
+CREATE DATABASE IF NOT EXISTS \`OrmsDatabase\` /*!40100 DEFAULT CHARACTER SET latin1 */;
+CREATE DATABASE IF NOT EXISTS \`OrmsLog\` /*!40100 DEFAULT CHARACTER SET latin1 */;
+EOIF
+echo "Successfully created ORMS DBs"
+echo "Granting privileges to DB user..."
+MYSQL_PWD=${DB_ROOT_PASSWORD} mariadb --protocol tcp --skip-ssl --user ${DB_ROOT_USER} --host ${DB_HOST} --port ${DB_PORT} <<'EOIF'
+GRANT ALL PRIVILEGES ON \`OrmsDatabase\`.* TO '$DB_USER'@'%';
+GRANT ALL PRIVILEGES ON \`OrmsLog\`.* TO '$DB_USER'@'%';
+FLUSH PRIVILEGES;
+EOIF
+echo "Successfully granted privileges to DB user"
+echo "Done!"
+EOF
